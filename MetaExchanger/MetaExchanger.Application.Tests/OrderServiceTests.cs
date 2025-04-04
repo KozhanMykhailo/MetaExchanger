81f4edcf4ae0ab85e43dd6b9e061301084b33e74
Kozhan Mykhailo <gatewayz87k@gmail.com>
2025-04-04 14:41:00 +0300

    #commit 11 - refactoring of CryptoExchangeService

--- MetaExchanger/MetaExchanger.Application.Tests/OrderServiceTests.cs
+++ MetaExchanger/MetaExchanger.Application.Tests/OrderServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-using MetaExchanger.Application.Common;
+using FluentValidation;
 using MetaExchanger.Application.Domain;
 using MetaExchanger.Application.Infrastructure;
 using MetaExchanger.Application.Services;
@@ -9,12 +8,10 @@ using NSubstitute;
 namespace MetaExchanger.Application.Tests
 {
     [TestFixture]
-    public class CryptoExchangeServiceTests
+    public class OrderServiceTests
     {
         private ApplicationDbContext _dbContext;
-        private ICryptoExchangeService _cryptoExchangeService;
         private IOrderService _orderService;
-        private IValidator<DomainOrder> _orderValidator;
 
         [SetUp]
         public void SetUp()
@@ -23,39 +20,50 @@ namespace MetaExchanger.Application.Tests
                 .UseInMemoryDatabase(databaseName: "TestDb")
                 .Options;
 
-            _dbContext = new ApplicationDbContext(options);
+            _dbContext =  new ApplicationDbContext(options);
 
             // add test data
             var crGuid = Guid.NewGuid();
-            _dbContext.CryptoExchanges.Add(new Models.CryptoExchange() { Id = crGuid, AcqTime = DateTime.UtcNow, BalanceBTC = 20, BalanceEUR = 20000 });
+            _dbContext.CryptoExchanges.Add(new Models.CryptoExchange() { Id = crGuid,  AcqTime = DateTime.UtcNow, BalanceBTC = 20, BalanceEUR = 20000});
             _dbContext.Orders.Add(new Models.Order() { Id = Guid.NewGuid(), Time = DateTime.UtcNow, CryptoExchangeId = crGuid, Type = "Buy", Amount = 1.5667m, Kind = "Limit", Price = 1.456m });
             _dbContext.Orders.Add(new Models.Order() { Id = Guid.NewGuid(), Time = DateTime.UtcNow, CryptoExchangeId = crGuid, Type = "Buy", Amount = 1.5667m, Kind = "Limit", Price = 1.856m });
             _dbContext.Orders.Add(new Models.Order() { Id = Guid.NewGuid(), Time = DateTime.UtcNow, CryptoExchangeId = crGuid, Type = "Sell", Amount = 6.5667m, Kind = "Limit", Price = 6.456m });
             _dbContext.SaveChanges();
 
-            _orderService = Substitute.For<IOrderService>();
-            _orderService.ProcessAsBuy(Arg.Any<DomainOrder>(), Arg.Any<List<DomainOrder>>()).Returns(new Error("CryptoExchanges don't have enough BTC for dealing, not enough:"));
-            _orderService.ProcessAsSell(Arg.Any<DomainOrder>(), Arg.Any<List<DomainOrder>>()).Returns(Task.FromResult(Result.Empty));
-
-            _orderValidator = Substitute.For<IValidator<DomainOrder>>();
-            _orderValidator.Validate(Arg.Any<DomainOrder>()).Returns(new FluentValidation.Results.ValidationResult());
-
-            _cryptoExchangeService = new CryptoExchangeService(_orderValidator, _dbContext, _orderService);
+            _orderService = new OrderService(_dbContext);            
         }
 
         [Test]
-        public async Task Get_Best_Execution_For_Buying_Failed()
+        public async Task Process_As_Selling()
         {
             // Arrange
-            var expectedAmount = 25;
-            var domainOrder = new DomainOrder() { Amount = expectedAmount, Type = Common.OperationType.Buy, Id = Guid.NewGuid(), Kind = Common.Kind.Limit, Time = DateTime.Now };
+            var domainOrder = new DomainOrder() { Amount = 1, Type = Common.OperationType.Sell, Id = Guid.NewGuid(), Kind = Common.Kind.Limit, Time = DateTime.Now };
+            var expectedPrice = 1.856m;
+            var ordersListResult = new List<DomainOrder>();
 
             // Act
-            var result = await _cryptoExchangeService.GetBestExecutionAsync(domainOrder);
+            var result = await _orderService.ProcessAsSell(domainOrder, ordersListResult);
 
             // Assert
-            Assert.That(result.Ok, Is.EqualTo(false));
-            Assert.IsTrue(result.Error!.Message.StartsWith("CryptoExchanges don't have enough BTC for dealing, not enough:"));
+            Assert.That(result.Ok, Is.EqualTo(true));
+            Assert.That(expectedPrice, Is.EqualTo(ordersListResult.First().Price!.Value));
+        }
+
+        [Test]
+        public async Task Process_As_Buying()
+        {
+            // Arrange
+            var expectedAmount = 2;
+            var domainOrder = new DomainOrder() { Amount = expectedAmount, Type = Common.OperationType.Buy, Id = Guid.NewGuid(), Kind = Common.Kind.Limit, Time = DateTime.Now };
+            var ordersListResult = new List<DomainOrder>();
+
+            // Act
+            var result = await _orderService.ProcessAsBuy(domainOrder, ordersListResult);
+
+            // Assert
+            Assert.That(result.Ok, Is.EqualTo(true));
+            Assert.That(ordersListResult.Count(), Is.EqualTo(1));
+            Assert.That(ordersListResult.First().Amount, Is.EqualTo(expectedAmount));
         }
 
         [TearDown]
---
+++ MetaExchanger/MetaExchanger.Application/Services/OrderService.cs
@@ -0,0 +1,117 @@
+﻿using MetaExchanger.Application.Common;
+using MetaExchanger.Application.Domain;
+using MetaExchanger.Application.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaExchanger.Application.Services
+{
+    /// <summary>
+    /// Domain order service.
+    /// </summary>
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result> ProcessAsBuy(DomainOrder requestDomainOrder, List<DomainOrder> responseResult)
+        {
+            var cryptoExchangeStates = new Dictionary<Guid, decimal>();
+
+            var remainingAmountBuy = requestDomainOrder.Amount;
+            var allOrders = await _dbContext.Orders.Include(o => o.CryptoExchange)
+                .Where(o => o.Type == "Sell")
+                .OrderBy(o => o.Price).ThenBy(o => o.Time)
+                .ToListAsync();
+
+            foreach (var order in allOrders)
+            {
+                //check meta balance during current request handling
+                if (cryptoExchangeStates.TryGetValue(order.CryptoExchangeId, out decimal btcAvailabel))
+                {
+                    //check - is it enough BTC balance of some CryptoExchange to make some operation with bids(type 'Sell')
+                    if (btcAvailabel < order.Amount && btcAvailabel < remainingAmountBuy) continue;
+                }
+
+                //if it's first iteration and CryptoExchange has enough btc balance - return as good deal
+                if (order.Amount >= remainingAmountBuy)
+                {
+                    var domainOrder = order.Convert();
+                    //if CryptoExchange has Bid('Sell' with btc balance > buyAmountRemaining) but does not have enough btc balance
+                    if (!cryptoExchangeStates.ContainsKey(order.CryptoExchangeId) && order.CryptoExchange.BalanceBTC < remainingAmountBuy)
+                    {
+                        //store temporary state of CryptoExchange BTC balance
+                        //take MAX BTC from CryptoExchange, in this case take all available balance
+                        cryptoExchangeStates.Add(order.CryptoExchangeId, 0);
+                        var order1 = order.Convert();
+                        order1.Amount = order.CryptoExchange.BalanceBTC;
+                        responseResult.Add(order1);
+                        remainingAmountBuy -= order.CryptoExchange.BalanceBTC;
+                        LogToConsole(order1);
+                        continue;
+                    }
+
+                    domainOrder.Amount = remainingAmountBuy;
+                    remainingAmountBuy = 0;
+
+                    responseResult.Add(domainOrder);
+                    LogToConsole(domainOrder);
+                    break;
+                }
+
+                //store temporary state of CryptoExchange BTC balance
+                //first add occures with '- order.Amount'
+                if (!cryptoExchangeStates.TryAdd(order.CryptoExchangeId, order.CryptoExchange.BalanceBTC - order.Amount))
+                {
+                    var currentBtcValue = cryptoExchangeStates.GetValueOrDefault(order.CryptoExchangeId);
+                    cryptoExchangeStates[order.CryptoExchangeId] = currentBtcValue - order.Amount;
+                }
+
+                remainingAmountBuy -= order.Amount;
+                var suggestedOrder = order.Convert();
+
+                responseResult.Add(suggestedOrder);
+                LogToConsole(suggestedOrder);
+            }
+
+            if (remainingAmountBuy > 0)
+                return new Error($"CryptoExchanges don't have enough BTC for dealing, not enough: {remainingAmountBuy} BTC");
+
+            return Result.Empty;
+        }
+
+        public async Task<Result> ProcessAsSell(DomainOrder domainOrder, List<DomainOrder> responseResult)
+        {
+            var sellAmount = domainOrder.Amount;
+            var order = await _dbContext.Orders.Include(o => o.CryptoExchange)
+                .Where(o => o.Type == "Buy" && o.CryptoExchange.BalanceEUR >= (o.Price * sellAmount))
+                .OrderByDescending(o => o.Price)
+                .FirstOrDefaultAsync();
+
+            if (order is null)
+                return new Error("Order with the highest price not found");
+
+            domainOrder.Price = order.Price;
+            domainOrder.CryptoExchangeId = order.CryptoExchange.Id;
+            //just create new order in DB
+            var newOrder = domainOrder.Convert();
+
+            _dbContext.Orders.Add(newOrder);
+
+            responseResult.Add(domainOrder);
+            LogToConsole(domainOrder);
+            return Result.Empty;
+        }
+
+        private void LogToConsole(DomainOrder order)
+        {
+            if (order.Type == OperationType.Sell)
+                Console.WriteLine($"\nThe best price for selling\nPrice: '{order.Price}', CryptoExchange ID: '{order.Id}'\n");
+            else
+                Console.WriteLine($"\nSuggested to Buy next:\nPrice: '{order.Price}', Amount: '{order.Amount}' CryptoExchange ID: '{order.Id}'\n");
+        }
+    }
+}
