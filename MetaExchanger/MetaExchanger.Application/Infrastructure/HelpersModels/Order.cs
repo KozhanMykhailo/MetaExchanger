1a8fc614dafe2606e6489ff5b051a4d2ffc858aa
Kozhan Mykhailo <gatewayz87k@gmail.com>
2025-04-04 12:22:22 +0300

    #commit 7 - DbInitializer refactoring

---
+++ MetaExchanger/MetaExchanger.Application/Infrastructure/HelpersModels/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace MetaExchanger.Application.Infrastructure.HelpersModels
+{
+    /// <summary>
+    /// Class helper for deserialization procces during DB initialize. Main entity
+    /// </summary>
+    public class Order
+    {
+        public object Id { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Type { get; set; }
+
+        public string Kind { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ MetaExchanger/MetaExchanger.Application/Infrastructure/HelpersModels/Root.cs
@@ -0,0 +1,11 @@
+﻿namespace MetaExchanger.Application.Infrastructure.HelpersModels
+{
+    /// <summary>
+    /// Class helper for deserialization procces during DB initialize. Contains bids that contains a order.
+    /// </summary>
+    public class Root
+    {
+        public DateTime AcqTime { get; set; }
+        public List<Bid> Bids { get; set; }
+    }
+}
